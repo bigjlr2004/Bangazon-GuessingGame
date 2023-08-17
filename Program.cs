479bb545f3a136dde452e43a5462ce73e8c786c8
John Roberts <bigjlr2004@yahoo.com>
2023-08-17 07:10:19 -0500

    Phase 2 Completed

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 
+// Create a variable to contain the secret number. This number should be hard-coded for now. 42 is a nice number.
+int secretNumber = 42;
 // Display a message to the user asking them to guess the secret number.
 Console.WriteLine("Lets play a game.");
     Console.WriteLine("-----------------");
@@ -8,8 +10,17 @@ Console.WriteLine("Lets play a game.");
     Console.Write("Guess the secret number?  ");
 // Take the user's guess as input and save it as a variable.
     int userGuess = int.Parse(Console.ReadLine());
-// Display the user's guess back to the screen.
+
+    
+// Compare the user's guess with the secret number.
+// Display a success message if the guess is correct, otherwise display a failure message.
+    if(userGuess == secretNumber){
     Console.WriteLine($"{userGuess} is a good guess.");
+    }
+    else
+    {
+    Console.WriteLine($"{userGuess} is a not a good guess.");
+    }
 
 
 
